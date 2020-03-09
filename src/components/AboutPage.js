648c7abbec7271e70f524dd5ba0f17f355dc505a
yaqueen887976 <yaqinwang@email.arizona.edu>
2020-03-09 11:04:20 -0600

    first commit

---
+++ src/components/AboutPage.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-lone-blocks */
+import React from "react";
+
+//creating component using class
+//work with function component, easy towork within classes
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>About</h1>
+        <p>This app uses React</p>
+      </>
+    );
+  }
+}
+
+export default AboutPage;
