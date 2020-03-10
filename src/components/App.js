6fdd699372b7b86e6a98a7b93331121f39c73788
yaqueen887976 <yaqinwang@email.arizona.edu>
2020-03-09 21:54:51 -0600

    update App.js, CoursePage, Header

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import Header from "./common/Header";
-
+import CoursePage from "./CoursesPage";
 function App() {
   function getPage() {
     const router = window.location.pathname;
+    if (router === "/courses") return <CoursePage />;
     if (router === "/about") return <AboutPage />;
     return <HomePage />;
   }
