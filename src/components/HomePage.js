509732d67001257ceb4613433984afec629dfb27
yaqueen887976 <yaqinwang@email.arizona.edu>
2020-03-10 11:20:51 -0600

    try for link and navlink

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 //create component using function
 function HomePage() {
@@ -6,7 +7,10 @@ function HomePage() {
     <div className="jumbotron">
       <h1>Pluralsight Administration</h1>
       <p>React, Flux and React Router for ultra-responsive web apps.</p>
-      <a href="/about">About</a>
+      {/*<a href="/about">About</a>*/}
+      <Link to="about" className="btn btn-primary">
+        About
+      </Link>
     </div>
   );
 }
